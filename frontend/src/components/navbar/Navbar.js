881d9588dfa6f06bb624e5f2fb7296126f392f84
manish <manishbisht9711@gmail.com>
2022-10-31 20:34:12 +0530

    chores: nav nav on page load mobile

--- frontend/src/components/navbar/Navbar.js
+++ frontend/src/components/navbar/Navbar.js
@@ -9,26 +9,38 @@ const Navbar = () => {
       <button
         aria-expanded={toggle}
         aria-label="toggle-menu"
-        onClick={() => settoggle(!toggle)}
+        onClick={() => {
+          settoggle(!toggle);
+        }}
         className="togglebtn"
       >
         <ion-icon name="menu-outline"></ion-icon>
       </button>
       <ul className="navlinks">
         <li>
-          <a href="/">home</a>
+          <a onClick={() => settoggle(false)} href="/">
+            home
+          </a>
         </li>
         <li>
-          <a href="#about">about</a>
+          <a onClick={() => settoggle(false)} href="#about">
+            about
+          </a>
         </li>
         <li>
-          <a href="#projects">projects</a>
+          <a onClick={() => settoggle(false)} href="#projects">
+            projects
+          </a>
         </li>
         <li>
-          <a href="#skills">skills</a>
+          <a onClick={() => settoggle(false)} href="#skills">
+            skills
+          </a>
         </li>
         <li>
-          <a href="#contact">contact</a>
+          <a onClick={() => settoggle(false)} href="#contact">
+            contact
+          </a>
         </li>
       </ul>
     </nav>
